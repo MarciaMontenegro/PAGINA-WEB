7f3a0ade57fa6df22a41c54380a458296c81acbf
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2022-05-01 11:21:42 -0500

    conexión urls

--- application.py
+++ application.py
@@ -1,1 +1,25 @@
-1
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/") 
+def inicio():
+  return render_template("inicio.html")
+
+@app.route("/productos") 
+def productos():
+  return render_template("productos.html")
+
+
+@app.route("/login") 
+def login():
+  return render_template("Login.html")
+
+@app.route("/register") 
+def register():
+  return render_template("Register.html")
+
+@app.route("/cart") 
+def cart():
+  return render_template("Cart.html")
